8d0aef654ebe3a2bb34518ccd3a2b153ddbaee32
LEJS-56 <luciano.ejs56@gmail.com>
2026-04-07 04:13:03 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <footer>
-	<p class="footer-button"><button><a style="text-decoration:none;color:white;" href=#>Retourner en haut</a></button>Copyright &copy; 2024-2025 | SONNE ELABA Joseph Luciano</p><br>
 	<nav class="menu-bas">
 		<ul>
 			<li><a href="index.php"><i class="ri-menu-line"></i></a></li>
